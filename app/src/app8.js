7e50395273ca682aff59796e6225bd9bba3d12b6
Robert Lovei <loveirobert@gmail.com>
2020-02-21 14:07:33 +0100

    fix lint errors

--- app/src/app8.js
+++ app/src/app8.js
@@ -2,8 +2,9 @@ const AWS = require('aws-sdk');
 const moment = require('moment');
 
 exports.lambdaHandler = async () => {
+  let response;
   const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-  const [, , resource, region, iamUser, resourceName] = process.env.MyQARN.split(':');
+  const [, , resource, region, iamUser, resourceName] = process.env.MyQARN.split(':'); // eslint-disable-line
   const queueUrl = `https://${resource}.${region}.amazonaws.com/${iamUser}/${resourceName}`;
   try {
     const params = {
@@ -11,7 +12,7 @@ exports.lambdaHandler = async () => {
       QueueUrl: queueUrl,
     };
 
-    await new Promise((res, rej) => {
+    await new Promise((res) => {
       sqs.sendMessage(params, function(msgError, data) {
         if (msgError) {
           console.log("Error", msgError);
--- app/src/logic/myq-message-handler.js
+++ app/src/logic/myq-message-handler.js
@@ -1,4 +1,4 @@
-exports.lambdaHandler = async ({ Records: [message] }, context) => {
+exports.lambdaHandler = async ({ Records: [message] }) => {
   console.log('Received message', JSON.stringify(message, null, 2));
   console.log('Message body: ', message.body);
 };
